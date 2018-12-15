a6c128224f3707e200d9c39a97535530a3ecbdb4
Carlos Azaustre <cazaustre@gmail.com>
2018-12-15 09:18:03 +0100

    testing with mocks

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 'use strict'
 
-const admin = require('firebase-admin')
-const serviceAccount = require('./config/key.json')
+const db = require('./lib/db')
+const Profile = require('./lib/profile')
 
-const DATABASE_NAME = 'developars-club'
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${DATABASE_NAME}.firebaseio.com`
-})
-
-const db = admin.firestore()
-
-db.settings({ timestampsInSnapshots: true })
-
-module.exports = db
+module.exports = {
+  profile: new Profile(db)
+}
--- tests/database.test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { dbStub } = require('./helpers')
-
-// Tests
-test('Database', async () => {
-  expect(dbStub).toBeDefined()
-})
